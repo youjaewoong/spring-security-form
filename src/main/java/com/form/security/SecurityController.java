57fc53a0bef690e3afb19d65ee8205d575125ad9
gos1004 <nnv711@gmail.com>
2022-10-06 23:22:08 +0900

    create project

---
+++ src/main/java/com/form/security/SecurityController.java
@@ -0,0 +1,38 @@
+package com.form.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+	@GetMapping
+	public String index() {
+		return "home";
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin/pay")
+	public String adminPay() {
+		return "adminPay";
+	}
+	
+	@GetMapping("/admin/**")
+	public String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/denied")
+	public String denied() {
+		return "Access is denied";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
